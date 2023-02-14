4ecf9baa771939f8229a026af37c6fb773d46531
wegulohillary <wegulohillary@gmail.com>
2023-02-14 08:07:16 -0800

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,16 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """ retrieve one object"""
+        self.reload()
+        key = cls + '.' + id
+        obj = self.__objects.get(key)
+        return obj
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        self.reload()
+        obj_dict = self.all(cls)
+        return len(obj_dict)
