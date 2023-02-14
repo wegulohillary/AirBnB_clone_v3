e61dcc0e9127391b186c593a8dfee8c7672522f7
wegulohillary <wegulohillary@gmail.com>
2023-02-14 08:20:51 -0800

    tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,21 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_get(self):
+        """ test get function """
+        state = State(name='Alaska')
+        models.storage.new(state)
+        models.storage.save()
+        self.assertEqual(models.storage.get("State", state.id).id, state.id)
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_c(self):
+        """ test count fuction """
+        s1 = models.storage.count("State")
+        s2 = State(name='Florida')
+        models.storage.new(s2)
+        models.storage.save()
+        s2 = models.storage.count("State")
+        self.assertEqual(s1, s2 - 1)
